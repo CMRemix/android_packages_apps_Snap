d43ee100673f6ac4c73c34cdf4839147fc4c6545
Nikita <nikich340@gmail.com>
2016-02-13 19:48:28 +1000

    Snap: small fixes for HDR-related code
    Change-Id: Ia432d3bf4b7e17fcd7f0cb1cff4d352d2aae808b

--- src/com/android/camera/PhotoMenu.java
+++ src/com/android/camera/PhotoMenu.java
@@ -134,8 +134,6 @@ public class PhotoMenu extends MenuController
         if (group.findPreference(CameraSettings.KEY_CAMERA_HDR) != null) {
             mHdrSwitcher.setVisibility(View.VISIBLE);
             initSwitchItem(CameraSettings.KEY_CAMERA_HDR, mHdrSwitcher);
-        } else {
-            mHdrSwitcher.setVisibility(View.INVISIBLE);
         }
 
         mOtherKeys = new String[] {
